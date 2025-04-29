704d8c651db2166f396b868bcd4c5c8e2e6d6aa1
Sam <lemon.bread5120@gmail.com>
2025-04-29 23:51:14 +0800

    added utility function for signUp and cleaned up the register page

---
+++ lib/auth/actions.ts
@@ -0,0 +1,29 @@
+import {createClient} from "@lib/supabase/server";
+import {Tables, TablesInsert, TablesUpdate} from "@/database.types";
+
+type SeniorUser = Tables<"user_senior_info">
+type SeniorUserInsert = TablesInsert<"user_senior_info">
+
+export async function signUpWithEmailAndPassword(
+    email: string,
+    password: string,
+) {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+    });
+    if (error) {
+        return { data, error: error.message };
+    }
+    return { data, error: null };
+}
+
+export async function signOut() {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+        return { error: error.message };
+    }
+    return { error: null };
+}

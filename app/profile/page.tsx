87ecb5e8e55f112e517e48b248906e1064ddda78
goodmorning1226 <allensung.taiwan@gmail.com>
2025-05-08 18:52:03 +0800

    profile page PersonInfo

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,19 +2,11 @@ import React from "react";
 import WishBox from "@components/WishBox/WishBox";
 import SearchBar from "@components/profile_page/SearchBar";
 import PersonInfo from "@components/profile_page/PersonInfo";
-import CareerMap from "@components/profile_page/CareerMap";
-import QA from "@components/profile_page/QA";
-import CommentInput from "@components/profile_page/CommentInput";
 
 const Page = () => {
   return (
     <div>
-      <div className="w-full h-[100px] min-h-[100px]" />
-      <SearchBar />
       <PersonInfo />
-      <CareerMap />
-      <QA />
-      <CommentInput />
       <WishBox />
     </div>
   );
--- components/shared_components/NaviBar/AccountBtn.tsx
+++ components/shared_components/NaviBar/AccountBtn.tsx
@@ -3,59 +3,63 @@
 import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/navigation";
-import {signOut} from "@lib/auth/actions";
-import {useUser} from "@/hook/use-user"; // or however you create your client
+import { signOut } from "@lib/auth/actions";
+import { useUser } from "@/hook/use-user"; // or however you create your client
 
 interface Props {
-    currentPage: number;
-    loggedIn: boolean;
+  currentPage: number;
+  loggedIn: boolean;
 }
 
 const AccountBtn = ({ currentPage, loggedIn }: Props) => {
-    const router = useRouter();
-    const {refetchUser} = useUser();
+  const router = useRouter();
+  const { refetchUser } = useUser();
 
-    const handleLogout = async () => {
-        await signOut();
-        refetchUser();
-        router.refresh(); // refresh to update session state
-        router.push("/login");
-    };
+  const handleLogout = async () => {
+    await signOut();
+    refetchUser();
+    router.refresh(); // refresh to update session state
+    router.push("/login");
+  };
 
-    return (
-        <div className="flex items-center ml-[40px]">
-            {loggedIn ? (
-                <button
-                    onClick={handleLogout}
-                    className={
-                        "text-base font-bold text-nowrap hover:text-black transition-all duration-200 ease-in" +
-                        (currentPage === 5 ? " text-black" : " text-[#979797]")
-                    }
-                >
-                    登出
-                </button>
-            ) : (
-                <Link
-                    className={
-                        "text-base font-bold text-nowrap hover:text-black transition-all duration-200 ease-in" +
-                        (currentPage === 5 ? " text-black" : " text-[#979797]")
-                    }
-                    href="/login"
-                >
-                    登入/註冊
-                </Link>
-            )}
+  return (
+    <div className="flex items-center ml-[40px]">
+      {loggedIn ? (
+        <button
+          onClick={handleLogout}
+          className={
+            "text-base font-bold text-nowrap hover:text-black transition-all duration-200 ease-in" +
+            (currentPage === 5 ? " text-black" : " text-[#979797]")
+          }
+        >
+          登出
+        </button>
+      ) : (
+        <Link
+          className={
+            "text-base font-bold text-nowrap hover:text-black transition-all duration-200 ease-in" +
+            (currentPage === 5 ? " text-black" : " text-[#979797]")
+          }
+          href="/login"
+        >
+          登入/註冊
+        </Link>
+      )}
 
-            <div
-                className="w-[40px] h-[40px] rounded-full ml-4"
-                onClick={() => {
-                    router.push(loggedIn ? "/register/finish" : "/login");
-                }}
-            >
-                <img className="w-full h-full cursor-pointer" src="/NaviBar/avatar.svg" alt="avatar" />
-            </div>
-        </div>
-    );
+      <div
+        className="w-[40px] h-[40px] rounded-full ml-4"
+        onClick={() => {
+          router.push(loggedIn ? "/profile" : "/login=");
+        }}
+      >
+        <img
+          className="w-full h-full cursor-pointer"
+          src="/NaviBar/avatar.svg"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AccountBtn;

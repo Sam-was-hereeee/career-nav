9d3a4382423a79434a71c6b5c1061c6e7b9ab0f5
goodmorning1226 <allensung.taiwan@gmail.com>
2025-05-08 20:40:52 +0800

    Profile page Experience Box

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,11 +2,34 @@ import React from "react";
 import WishBox from "@components/WishBox/WishBox";
 import SearchBar from "@components/profile_page/SearchBar";
 import PersonInfo from "@components/profile_page/PersonInfo";
+import ExperienceBox from "@/components/profile_page/ExperienceBox";
+import ExperienceEntry from "@/components/profile_page/ExperienceEntry";
 
 const Page = () => {
   return (
     <div>
       <PersonInfo />
+      <div className="flex flex-col h-auto py-10 gap-8 items-center">
+        <ExperienceBox type="工作經驗">
+          <ExperienceEntry
+            experience="IBM"
+            occupation="Product Marketing Specialist"
+            period="2022-Present"
+          />
+          <ExperienceEntry />
+        </ExperienceBox>
+
+        <ExperienceBox type="實習經驗">
+          <ExperienceEntry />
+          <ExperienceEntry />
+        </ExperienceBox>
+
+        <ExperienceBox type="社團經驗"></ExperienceBox>
+
+        <ExperienceBox type="其他經驗">
+          <ExperienceEntry />
+        </ExperienceBox>
+      </div>
       <WishBox />
     </div>
   );

4c1b47f82ec71eb96379831efd286967ea925e47
goodmorning1226 <allensung.taiwan@gmail.com>
2025-05-15 14:08:30 +0800

    simplified hero section for mobile -2

--- components/main_page/JoinSeniorBar.tsx
+++ components/main_page/JoinSeniorBar.tsx
@@ -14,7 +14,12 @@ const JoinSeniorBar = () => {
       {/* Text Content */}
       <div className="w-full max-w-3xl flex flex-col text-black text-left">
         <h3 className="text-xl sm:text-2xl lg:text-[24px] font-bold text-[#474F56] mb-4 leading-snug">
-          職屬：攜手踏上屬於人文專長人才的職涯探險
+          <span className="hidden sm:inline">
+            職屬：攜手踏上屬於人文專長人才的職涯探險
+          </span>
+          <span className="sm:hidden">
+            職屬：攜手踏上人文專長人才的職涯探險
+          </span>
         </h3>
 
         <p className="text-sm sm:text-base text-[#5F6368] leading-loose sm:leading-relaxed space-y-2 mb-5 sm:mb-0">
@@ -24,22 +29,23 @@ const JoinSeniorBar = () => {
           <br />
           <span className="hidden sm:inline">
             當全世界都在追逐新科技，選擇就讀文學院的我們，在研讀著幾世紀以前的文學、歷史、藝術。
-          </span>
-          <br className="hidden sm:block" />
-          <span className="hidden sm:inline">
+            <br />
             我們之中，有人熱愛文化與藝術；有人懷著對創作的興趣，渴望發揮創意長才。我們相信人文學科的價值。
-          </span>
-          <br className="hidden sm:block" />
-          <span className="hidden sm:inline">
+            <br />
             但當科技掛帥的世界都告訴我們，文組沒出路時，我們也不禁懷疑：在文學院的自己，未來在何方？
+            <br />
           </span>
-          <br className="hidden sm:block" />
-          <span className="font-bold block mt-2">
+          <span className="font-bold mt-2 hidden sm:block">
             職屬因此而誕生。
             <br />
             來自台大文學院的我們，希望連結文學院在學的學生和就業學長姐：
             <br />
             由職屬學長姐的職業經驗傳承，協助在學的學生，找到屬於文學院學生們的職涯航道。
+          </span>
+          <span className="font-bold mt-2 block sm:hidden">
+            職屬因此而誕生。
+            <br />
+            來自台大文學院的我們，希望連結在學的學生和就業學長姐：由職屬學長姐的職業經驗傳承，協助在學的學生，找到屬於文學院的職涯航道。
           </span>
         </p>
 

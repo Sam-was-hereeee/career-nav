98b5ffdc13921f972de176b9211737d16e55ddaa
CX330Blake <timchen975@gmail.com>
2025-05-17 02:26:36 +0800

    Fix redirecting

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 const InterviewPage = () => {
-    const router = useRouter();
-
     useEffect(() => {
         const url = "https://www.instagram.com/p/DHyHjYdyYtb/";
-        window.open(url, "_blank");
-        router.back();
+        window.location.href = url;
     }, []);
-
-    return null;
 };
 
 export default InterviewPage;

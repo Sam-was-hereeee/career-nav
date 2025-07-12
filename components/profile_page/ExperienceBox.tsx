6ff8c0879d34e0adb404de10be7f1951c36737ee
goodmorning1226 <allensung.taiwan@gmail.com>
2025-07-12 17:36:17 +0800

    profileBox顏色更改

--- components/profile_page/ExperienceBox.tsx
+++ components/profile_page/ExperienceBox.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ExperienceBox = ({ type, children }: Props) => {
   return (
     <div className="relative w-5/6 lg:w-3/4 h-auto pt-8 md:pt-10 border border-[#D7D8D9] rounded-md overflow-hidden">
-      <div className="flex justify-center items-center absolute w-full h-8 md:h-10 top-0 bg-[#D7D8D9]">
+      <div className="flex justify-center items-center absolute w-full h-8 md:h-10 top-0 bg-[#B2BEC180]">
         <h3 className="text-base">{type}</h3>
       </div>
       {children}
